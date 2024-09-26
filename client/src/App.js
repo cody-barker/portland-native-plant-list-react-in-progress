df71aff6c9923d5144f45a6b5fcedb9d7a60294e
Cody <codybarker.or@gmail.com>
2024-09-26 16:25:04 -0700

    refactor App, add NotFound comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,37 @@
-import "./styles/App.css";
-import { Routes, Route, useLocation } from "react-router-dom";
-import NavBar from "./components/NavBar";
-import Home from "./pages/Home";
-import AllPlants from "./pages/AllPlants";
-import PlantForm from "./components/PlantForm";
-import { SearchProvider } from "./SearchContext";
-import Footer from "./components/Footer";
-import Login from "./pages/Login";
-import { AdminProvider } from "./AdminContext";
-import { ToastContainer } from "react-toastify";
-import PlantDetail from "./pages/PlantDetail";
-import UpdatePlantForm from "./components/UpdatePlantForm";
-import ReactGA from "react-ga";
 import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
+import "./styles/App.css"; // Styles
+import NavBar from "./components/NavBar"; // Navigation Bar
+import Footer from "./components/Footer"; // Footer Component
+import Home from "./pages/Home"; // Home Page
+import AllPlants from "./pages/AllPlants"; // All Plants Page
+import PlantDetail from "./pages/PlantDetail"; // Plant Detail Page
+import PlantForm from "./components/PlantForm"; // Plant Form Component
+import UpdatePlantForm from "./components/UpdatePlantForm"; // Update Plant Form Component
+import Login from "./pages/Login"; // Login Page
+import NotFound from "./pages/NotFound"
+import { SearchProvider } from "./SearchContext"; // Search Context
+import { AdminProvider } from "./AdminContext"; // Admin Context
+
+import ReactGA from "react-ga"; // Google Analytics
+
+const TRACKING_ID = "G-2DW60JMQR5";
+
+const AppRoutes = () => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/plants" element={<AllPlants />} />
+    <Route path="/plants/:id" element={<PlantDetail />} />
+    <Route path="/plants/:id/edit" element={<UpdatePlantForm />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/plants/new" element={<PlantForm />} />
+    <Route path="*" element={<NotFound />} />
+  </Routes>
+);
 
 function App() {
-  const TRACKING_ID = "G-2DW60JMQR5";
   ReactGA.initialize(TRACKING_ID);
   const location = useLocation();
 
@@ -27,23 +43,7 @@ function App() {
     <SearchProvider>
       <AdminProvider>
         <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/plants" element={<AllPlants />} />
-          <Route path="/plants/:id" element={<PlantDetail />} />
-          <Route path="/plants/:id/edit" element={<UpdatePlantForm />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/plants/new" element={<PlantForm />} />
-          <Route
-            path="*"
-            element={
-              <div className="vh85">
-                <h2 className="dark-green">404 Error - Page Not Found</h2>
-                <h3 className="dark-green"> Please Return to the Home Page</h3>
-              </div>
-            }
-          />
-        </Routes>
+        <AppRoutes />
         <Footer />
         <ToastContainer />
       </AdminProvider>
---
+++ client/src/pages/NotFound.js
@@ -0,0 +1,10 @@
+function NotFound() {
+  return (
+    <div className="vh85">
+      <h2 className="dark-green">404 Error - Page Not Found</h2>
+      <h3 className="dark-green">Please Return to the Home Page</h3>
+    </div>
+  );
+}
+
+export default NotFound;
